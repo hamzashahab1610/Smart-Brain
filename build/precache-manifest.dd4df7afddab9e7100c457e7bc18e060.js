976a13c5f0b99129815666ff63923f0df8bd5144
hamzashahab1610 <hamzashahab1610@gmail.com>
2019-09-27 23:35:26 -0700

    ...

---
+++ build/precache-manifest.dd4df7afddab9e7100c457e7bc18e060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27077b8ab5cc4260e00a3fa28d4be0d7",
+    "url": "/Smart-Brain/index.html"
+  },
+  {
+    "revision": "dfe28c8ac9248066114c",
+    "url": "/Smart-Brain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "dc87ee5f36a3818e92a6",
+    "url": "/Smart-Brain/static/css/main.5265e23b.chunk.css"
+  },
+  {
+    "revision": "dfe28c8ac9248066114c",
+    "url": "/Smart-Brain/static/js/2.f4cffd9b.chunk.js"
+  },
+  {
+    "revision": "dc87ee5f36a3818e92a6",
+    "url": "/Smart-Brain/static/js/main.3da0ea74.chunk.js"
+  },
+  {
+    "revision": "435c21ea7316d9407c5e",
+    "url": "/Smart-Brain/static/js/runtime-main.6a141c79.js"
+  }
+]);
